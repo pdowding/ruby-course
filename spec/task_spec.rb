1fbb0619b414fed6c4075adf73ac227fe2334446
pdowding <phil_dowding@hotmail.com>
2014-04-29 15:21:53 -0500

    Project Tests Started

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -5,3 +5,23 @@ describe 'Task' do
     expect(TM::Task).to be_a(Class)
   end
 end
+
+
+describe 'create_new_project' do
+  it can be created with a name do
+  end
+end
+
+describe 'create_new_project' do
+  it 'can automatically generate the new project a unique id' do
+   end
+  end
+
+  describe 'create_new_project' do
+    it ' can automatically assign the new project a unique id' do
+    end
+  end
+
+  describe 'create_new_project' do
+    it
+  end
