84597edccec4273633af9617d0ee4e3adfe65e6d
pdowding <phil_dowding@hotmail.com>
2014-04-29 15:09:35 -0500

    Project started

--- lib/task-manager.rb
+++ lib/task-manager.rb
@@ -1,8 +1,34 @@
 
 # Create our module. This is so other files can start using it immediately
 module TM
-end
+
 
 # Require all of our project files
 require_relative 'task-manager/task.rb'
 require_relative 'task-manager/project.rb'
+
+class Project
+
+
+def create_new_project
+  #A new project can be created with a name
+  #Must automatically generate and assign the new project a unique id
+  #A class variable can be used for this
+  #A project can retrieve a list of all complete tasks, sorted by creation date
+
+
+end
+
+
+def create_new_task
+  #A new task can be created with a project id, description and priority number
+  #A task can be marked as complete specified by id
+
+end
+
+
+
+
+end
+
+end
